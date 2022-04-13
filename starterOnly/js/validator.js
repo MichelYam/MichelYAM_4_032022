0412e246698b511ed18cd6c578b966cec75f56f1
MichelYAM <michel.yam92@gmail.com>
2022-04-13 12:37:51 +0200

    maj regex name

--- starterOnly/js/validator.js
+++ starterOnly/js/validator.js
@@ -23,7 +23,7 @@ let currentDate = new Date(); // get the current day
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // regex for handle special characters
-const regexName = /^[^@&"'`|°()!_$*€£`+=\/;?#]+$/;
+const regexName = /^[a-zA-Zéè\s]+$/;
 
 
 // check if the input is empty
